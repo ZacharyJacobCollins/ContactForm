c9fb89b8b108e477b7e18f18b42295603487c5f1
zacharyjacobcollins@gmail.com <zacharyjacobcollins@gmail.com>
2016-08-13 08:00:02 -0700

    updates

---
+++ js/components/contact.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#contact',
+    data: {
+        selected: '',
+        companies: [
+            { Name: 'Foresee', Email: '@foreseehr.com', src: 'images/foresee_2016.png' }, 
+            { Name: 'SSC', Email: '@SSC.com', src: 'fill in the other source icon here kris'},
+            { Name: 'Ted', Email: '@Ted.com', src: 'fill in the other source icon here kris'}
+        ]
+    }, 
+    methods: [
+
+    ]
+});
